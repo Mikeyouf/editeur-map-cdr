88805da586fa5935de2917425feef8a598614e06
mikael antoine <mikael antoine>
2018-05-14 22:36:31 +0200

    modif js

--- tchat.js
+++ tchat.js
@@ -9,6 +9,6 @@ window.onload = function () {
     //fonction qui ouvre la popup au clic
     const openPopup = document.getElementById("openPopup");
     openPopup.addEventListener("click", function() {
-        window.open("https://editeur-map-cdr.netlify.com/tchatpopup.html", "tchat CDR", 'menubar=yes, scrollbars=yes, top=20, left=500, width=360, height=380');
+        window.open("https://editeur-map-cdr.netlify.com/tchatpopup.html", "tchat CDR", 'menubar=yes, scrollbars=yes, top=20, left=500, width=370, height=430');
     });
 }
