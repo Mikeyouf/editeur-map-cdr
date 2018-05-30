c3781079ef4075336c3a8510e25d6f19371de6cf
Mikeyouf <mikeyouf@gmail.com>
2018-05-30 14:15:58 +0200

    test nation

--- tchatCdr/bouton.js
+++ tchatCdr/bouton.js
@@ -39,11 +39,12 @@ window.onload = function () {
     let match = joueursArray.filter(getClass);
     const indexJoueur = Math.floor(match.length/2);
     const player = match.splice(indexJoueur, indexJoueur +1).shift();
-    const playerClass = player.classList.split("_");
+    const playerArray = Array.from(player);
+    // const playerClass = player.classList;
     // const nation = player.classList.contains("rus") ? "Ru" : "Fr";
     // const nation = playerClass.indexOf("rus") != -1 ? "Ru" : "Fr";
 
-    console.log(player);
-    console.log(playerClass);
+    console.log(playerArray);
+    // console.log(playerClass);
     // console.log(nation);
 }
